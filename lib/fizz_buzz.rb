f8080f3dfbd3e08362a36cc7ae905776a543a565
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-10 14:53:29 -0600

    replaces multiples of 3 with Fixx, 5 with Buzz, and both with FizzBuzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,33 @@
 class FizzBuzz
   def initialize(high_limit)
     @high_limit = high_limit
+  end
+  def arrayify
+    numbers = 1.upto(@high_limit).to_a
 
-    def arrayify
-      1.upto(@high_limit).to_a
+    i = 0
 
+    while (i < numbers.length)
+      #do my replacements
+      if (is_multiple_of?(3, numbers[i]) && (is_multiple_of?(5, numbers[i])))
+        numbers[i] = "FizzBuzz"
+      end
+      if (is_multiple_of?(3, numbers[i]))
+        numbers[i] = "Fizz"
+      end
+
+      if (is_multiple_of?(5, numbers[i]))
+        numbers[i] = "Buzz"
+      end
+
+      i += 1
     end
+
+    numbers
+    end
+
+  def is_multiple_of?(divisible_by, number)
+    number % divisible_by == 0
+
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,19 +4,19 @@
 #multiple of 5 = buzz
 #multiple of 3 and 5 = fizzbuzz
 
-require "rspec/core"
-require "/Users/MikeMac/gschoolwork/intro_to_tdd/lib/fizz_buzz.rb"
+require 'rspec/core'
+
+require '/Users/MikeMac/gschoolwork/intro_to_tdd/lib/fizz_buzz.rb'
 
 describe FizzBuzz do
 
-  it "passes" do
-    fizzbuzz = FizzBuzz.new(5)
+  it "replaces multiples of 3 with Fizz and multiples of 5 with Buzz, and multiples of both by FizzBuzz" do
+    fizzbuzz = FizzBuzz.new(15)
 
-    expected = [1, 2, 3, 4, 5]
+    expected = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
 
     actual = fizzbuzz.arrayify
 
     expect(actual).to eq expected
-
-  end
+    end
 end
