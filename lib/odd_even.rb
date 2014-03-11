5dfc52ebb9d30ac3c2e4e5b39b261030ad0d5c19
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-11 15:02:14 -0600

    the program returns an array that replaces odd and even for the integer value

--- lib/odd_even.rb
+++ lib/odd_even.rb
@@ -8,7 +8,26 @@ class Count
   def run
 
   count_array = 1.upto(@num).to_a
+
+  i = 0
+    while i < count_array.length
+      if (is_even?(i))
+        count_array[i-1] = "even"
+      end
+      if (is_odd?(i))
+        count_array[i-1] = "odd"
+      end
+      i += 1
+    end
   count_array
   end
-end
 
+  def is_odd?(number)
+  number % 2 == 1
+
+  end
+  def is_even?(number)
+  number % 2 == 0
+
+  end
+end
--- spec/odd_even_spec.rb
+++ spec/odd_even_spec.rb
@@ -4,11 +4,11 @@ require '/Users/MikeMac/gSchoolWork/intro_to_tdd/lib/odd_even.rb'
 
 describe "Odd/Even" do
 
-  it "the program will return an array starting with 1 and ending with a number that's inputted" do
+  it "shows Odd or Even in the array instead of the actual number" do
     number_array = Count.new(10)
     number_array.run
 
-    expected_value = [1,2,3,4,5,6,7,8,9,10]
+    expected_value = ["odd","even","odd","even","odd","even","odd","even","odd","even"]
     actual_value = number_array.run
 
     expect(actual_value).to eq expected_value
